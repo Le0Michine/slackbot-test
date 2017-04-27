e534863d87c5874485e928716d1040ed81948f66
Le0Michine <leomichine@gmail.com>
2017-04-27 09:10:23 +0200

    update response format

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -4,20 +4,20 @@ admin.initializeApp(functions.config().firebase);
 
 exports.getTFSItemLinkById = functions.https.onRequest((request, response) => {
     const id = request.query.id;
-    const { token, text, team_id } = request.body;
+    const { token, text, team_id, response_url } = request.body;
 
     admin.database().ref('/tfs').child(team_id).on('value', snapshot => {
         const tfsUrl = snapshot.val() ? snapshot.val().url : '';
         const ref = `<${tfsUrl}${text}|Item ${text}>`;
-        response.send({ text: ref });
+        response.send({ response_type: "in_channel", text: ref });
     }, errorObject => {
-        response.send(id);
+        response.send({ response_type: "ephemeral", text: JSON.stringify(errorObject) });
     });
 });
 
 exports.setTFSBaseURL = functions.https.onRequest((request, response) => {
-    const { token, text, team_id } = request.body;
+    const { token, text, team_id, response_url } = request.body;
     admin.database().ref('/tfs').child(team_id).set({ url: text }).then(snapshot => {
-        response.send({ text: "Done, new url", attachments: [ { text: `<${text}>` } ] });
+        response.send({ response_type: "in_channel", text: "Done, new url", attachments: [ { text: `<${text}>` } ] });
     });
 });
