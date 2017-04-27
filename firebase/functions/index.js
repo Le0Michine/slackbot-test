6309c26c4795b323935ec50bffe40372ee310d30
Le0Michine <leomichine@gmail.com>
2017-04-27 16:18:54 +0200

    support several ids, added log messages

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -5,10 +5,18 @@ admin.initializeApp(functions.config().firebase);
 exports.getTFSItemLinkById = functions.https.onRequest((request, response) => {
     const id = request.query.id;
     const { token, text, team_id, response_url } = request.body;
+    console.log(`called with ${JSON.stringify(request.body)}`);
+    const ids = text.split(/[., ]/).map(x => +x).filter(x => !!x);
+
+    if (!text || /^ *$/.test(text) || !ids.length) {
+        response.send({ response_type: "ephemeral", text: "you should pass comma separated list of tfs item ids" });
+
+        return;
+    }
 
     admin.database().ref('/tfs').child(team_id).on('value', snapshot => {
         const tfsUrl = snapshot.val() ? snapshot.val().url : '';
-        const ref = `<${tfsUrl}${text}|Item ${text}>`;
+        const ref = ids.map(x => `<${tfsUrl}${x}|Item ${x}>`).join(', ');
         response.send({ response_type: "in_channel", text: ref });
     }, errorObject => {
         response.send({ response_type: "ephemeral", text: JSON.stringify(errorObject) });
